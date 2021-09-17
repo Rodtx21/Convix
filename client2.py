708629002c892c02e6bd1351c06b1a456bfac5cf
Rodtx21 <52375679+Rodtx21@users.noreply.github.com>
2021-09-17 20:22:57 +0100

    Update client2.py

--- client2.py
+++ client2.py
@@ -1,4 +1,5 @@
 import socket
+from threading import *
 
 def get_string(r):
     a = input(r)
@@ -11,15 +12,27 @@ def get_string(r):
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("5.43.52.92", 1025))
+s.connect(("192.168.0.116", 1025))
 
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
 
+class sending(Thread):
+    def run(self):
+        while True:
+            m = get_string("")
+            s.send(bytes(m, "utf-8"))
+
+class receiving(Thread):
+    def run(self):
+        while True:
+            answer = s.recv(1024)
+            an = answer.decode("utf-8")
+            print(an)
+
 while True:
-    answer = s.recv(1024)
-    an = answer.decode("utf-8")
-    print(an)
-    
-    m = get_string("")
-    m = s.send(bytes(m, "utf-8"))
+    send = sending()
+    recei = receiving()
+
+    send.start()
+    recei.start()
